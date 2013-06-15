bd8d7127dc923978fbe2e73b7c7b01e2d96c510d
Dale Hamel <dale.hamel@srvthe.net>
2013-06-15 00:58:31 -0500

    Some post-merge goodies

--- dobuild.sh
+++ dobuild.sh
@@ -56,7 +56,12 @@ while true; do
             sourceforge=1
             ;;
         --user)
-            user=dalehamel
+            shift
+            if [ -z $1 ]; then
+                echo "user parameter missing!"
+                exit 1
+            fi
+            user="$1"
             ;;
         --force-pht-rebuild)
             force_pht_rebuild=1
@@ -85,25 +90,26 @@ if [ $devbuild -eq 1 ];then
     echo "This is a development build!"
 fi
 
-version=$1
-distroname="rasplex"
+scriptdir=$(cd `dirname $0` && pwd)
+outfilename="$distroname-RPi.arm-$version"
+tmpdir="$scriptdir/tmp"
+outimagename="$distroname-$version.img"
+outimagefile="$tmpdir/$outimagename"
+targetdir="$scriptdir/target"
+
+# set rasplex config
+echo "
+RASPLEX_VERSION=\"$version\"
+DISTRONAME=\"$distroname\"
+" > $scriptdir/config/rasplex
 
 sed s/SET_RASPLEXVERSION/"$version"/g $scriptdir/config/version.in > $scriptdir/config/version
-time PROJECT=RPi ARCH=arm make release || exit
-mkdir -p $tmpdir
-rm -rf $tmpdir/*
-cp $targetdir/$prefix-RPi.arm-$version.tar.bz2 $tmpdir
-echo "Extracting release tarball..."
-tar -xpjf $tmpdir/$prefix-RPi.arm-$version.tar.bz2 -C $tmpdir
-dd if=/dev/zero of=$outfile bs=1M count=1500
 
-
-echo "Creating SD image"
-cd $tmpdir/$prefix-RPi.arm-$version
 
 # build
 function build {
     echo "Building rasplex"
+
     source $scriptdir/config/version
     if [ $force_pht_update -eq 1 ]; then
         rm -r "sources/plexht"
@@ -205,42 +211,10 @@ function upload_sourceforge {
 build
 create_image
 
-if [ "$2" == "--dist" ];then
-
-    gzip $outfile
-
-    if [ "$3" == "--debug" ];then
-        echo "This is a debug dist"
-        autoupdate_dir=rasplexdev
-    else
-        autoupdate_dir=rasplex
-
-        echo "Distributing $prefix-RPi.arm-$version"
-
-        echo "Distributing update image to sourceforge mirror"
-        time scp  $tmpdir/$prefix-RPi.arm-$version.tar.bz2  dalehamel@frs.sourceforge.net:/home/frs/project/rasplex/autoupdate/$autoupdate_dir
-
-        echo "Setting latest on sourceforge mirror"
-        echo "$prefix-RPi.arm-$version" > latest
-        time scp latest dalehamel@frs.sourceforge.net:/home/frs/project/rasplex/autoupdate/$autoupdate_dir
-
-
-    fi
-
-    #To do: add proper mirror deployment
-    echo "Copying install archive to S3..."
-    time cp $archive /mnt/plex-rpi
- 
-    echo "Distributing install image to sourceforge mirror"
-    time scp $archive dalehamel@frs.sourceforge.net:/home/frs/project/rasplex/release/
-
-    echo "Setting bleeding on sourceforge mirror"
-    echo "http://sourceforge.net/projects/rasplex/files/release/$outname.gz/download" >bleeding
-    time scp bleeding dalehamel@frs.sourceforge.net:/home/frs/project/rasplex/release
-
-   
-
+if [ $sourceforge -eq 1 ]; then
+    upload_sourceforge
 fi
 
 echo "done."
+
 
